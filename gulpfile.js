323ec17213a2e98e76b2979ffa408983fb5c45d5
Scott Smith <stsmith@callutheran.edu>
2020-01-23 16:51:17 -0800

    added gulp-responsive to resize images

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var svgSprite = require('gulp-svg-sprite');
 var autoprefixer = require('gulp-autoprefixer');
+var responsive = require('gulp-responsive');
 
 // files
-let svgFiles = ['./src/svg-icons/general/*.svg', './src/svg-icons/home/*.svg'];
-let sassFiles = './src/sass/*.scss';
+const svgFiles = ['./src/svg-icons/general/*.svg', './src/svg-icons/home/*.svg'];
+const sassFiles = './src/sass/*.scss';
+const images = ['./src/images/uploads/*.jpg'];
 
 
 // SASS
@@ -63,13 +65,47 @@ function spriteCompile(cb) {
 }
 
 
+// resize images
+function resizeImages(cb){
+	cb();
+
+	gulp.src(images)
+		.pipe(
+			responsive(
+				{
+					'*.jpg': [
+						{
+							width: 200,
+							rename: { suffix: '-200' }
+						},
+						{
+							width: 400,
+							rename: { suffix: '-400' }
+						},
+						{
+							width: 800,
+							rename: { suffix: '-800' }
+						}
+					]
+				},
+				{
+					quality: 85
+				}
+			)
+		)
+		.pipe(gulp.dest('./build/images/resized'));
+
+}
+
+
 
 // gulp tasks
-exports.default = gulp.series(sassCompile, spriteCompile);
+exports.default = gulp.series(sassCompile, spriteCompile, resizeImages);
 
-exports.build = gulp.series(sassCompile, spriteCompile);
+exports.build = gulp.series(sassCompile, spriteCompile, resizeImages);
 
 exports.watch = function () {
-	gulp.watch('./src/sass/**/*.scss', sassCompile);
-	gulp.watch(svgFiles, spriteCompile);
+	gulp.watch('./src/sass/**/*.scss', { ignoreInitial: false }, sassCompile);
+	gulp.watch(svgFiles, { ignoreInitial: false }, spriteCompile);
+	gulp.watch(images, { ignoreInitial: false }, resizeImages);
 }
